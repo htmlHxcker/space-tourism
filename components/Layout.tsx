13a17d80ce986a9abebb3d19e03aab1971a9bc2c
htmlHxcker <clintonadeleke@gmail.com>
2021-11-05 00:10:25 +0200

    [feat]: Add Layout to improve Page Structure

---
+++ components/Layout.tsx
@@ -0,0 +1,15 @@
+import type { NextComponentType } from 'next'
+import Header from './Header'
+import Sidebar from './Sidebar'
+
+const Layout: NextComponentType = ({ children }) => {
+  return (
+    <div className="min-h-screen max-w-[1440px] mx-auto flex flex-col p-6 md:px-10 lg:px-14 md:py-0 lg:pt-10 overflow-hidden">
+      <Header />
+      <main className="h-full flex-auto lg:pl-28 lg:pr-16">{children}</main>
+      <Sidebar />
+    </div>
+  )
+}
+
+export default Layout
